60bf3f60f0a654aafb1a2a0ec960165d352e2d66
unknown <mariem9>
2022-05-03 22:23:22 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { MoralisProvider } from "react-moralis";
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MoralisProvider appId="y9HxbhqkozGb18ScXXd17KANCIBcNwQEhFVOo9tT" serverUrl="https://2vsvkzw90paz.usemoralis.com:2053/server">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MoralisProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
